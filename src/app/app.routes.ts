8ab6700cb170bddd8b8923ee715e93d769f42b07
ZV <wazolee@gmail.com>
2025-04-13 20:19:21 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,35 @@ import { CatalogComponent } from '@catalog/catalog.component';
 import { SearchComponent } from '@catalog/search/search.component';
 import { CartComponent } from '@shared/cart/cart.component';
 import { SquadCatalogComponent } from './squad/squad-catalog/squad-catalog.component';
+import { CART_OPTIONS_TOKEN, CartOptions, CartService } from '@catalog/cart.service';
 
 export const routes: Routes = [
   { path: 'catalog', component: CatalogComponent, title: "Catalog - Joe's Robot Shop" },
   { path: 'search', component: SearchComponent, title: "Search - Joe's Robot Shop" },
   { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" },
-  { path: 'squad/bot-squad', component: SquadCatalogComponent, title: "Bot Squad - Joe's Robot Shop" },
-  { path: 'squad/squad-cart', component: CartComponent, title: "Squad Cart - Joe's Robot Shop" },
+  {
+    path: 'squad',
+    children: [
+      { path: 'bot-squad', component: SquadCatalogComponent, title: "Bot Squad - Joe's Robot Shop" },
+      { path: 'squad-cart', component: CartComponent, title: "Squad Cart - Joe's Robot Shop" },
+    ],
+    providers: [
+      {
+        provide: CART_OPTIONS_TOKEN,
+        useValue: {
+          persistenceType: 'local',
+          persistentKey: 'squad-cart',
+        },
+        multi: false,
+      },
+      {
+        provide: CartService,
+        useFactory: (cartOptions: CartOptions) => { return new CartService(cartOptions); },
+        deps: [CART_OPTIONS_TOKEN]
+      },
+    ]
+  },
+
   { path: '', redirectTo: '/catalog', pathMatch: 'full' },
 ];
 
